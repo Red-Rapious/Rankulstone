542ddbdb94cca4d0914e2bf1e87f402fa0431518
Red Rapious <red.rapious@gmail.com>
2019-11-02 22:53:13 +0100

    Modifications on network
    Now works on a domestic network

--- Godot Project/Client.gd
+++ Godot Project/Client.gd
@@ -1,9 +1,17 @@
 extends Button
 
+func _ready():
+	get_tree().connect("connected_to_server", self, "connected")
+	get_tree().connect("connection_failed", self, "failed")
 
 func _on_Client_pressed():
 	var peer = NetworkedMultiplayerENet.new()
-	peer.create_client("82.255.203.114", 666)
+	peer.create_client("192.168.1.191", 666)
 	get_tree().set_network_peer(peer)
 	
 
+func connected():
+	print("Successfully connected!")
+	
+func failed():
+	print("Connection failed...")
--- Godot Project/Server.gd
+++ Godot Project/Server.gd
@@ -1,9 +1,13 @@
 extends Button
 
+func _ready():
+	get_tree().connect("network_peer_connected", self, "_player_connected")
+
 func _on_Server_pressed():
 	var peer = NetworkedMultiplayerENet.new()
-	peer.create_server(666, 2)
+	peer.create_server(666, 10)
 	get_tree().set_network_peer(peer)
+	print("Successfully created a server with IP: ", IP.get_local_addresses())
 	
 func _player_connected(id):
-	print("Player", id, "connected")
+	print("Player ", id, " is connected!")
