47361398b029ef95f267388b9c8ece14918c6396
Red Rapious <red.rapious@gmail.com>
2019-11-07 15:53:16 +0100

    Title Screen creation
    The base, imageless screen is functionnal. New functionnalities will be added later

---
+++ Godot Project/Scripts/Buttons/Fight_Button.gd
@@ -0,0 +1,4 @@
+extends Button
+
+func _on_Fight_Button_pressed():
+	get_tree().change_scene("Scenes/Menus/Lobby.tscn")
---
+++ Godot Project/Scripts/Buttons/Options_Button.gd
@@ -0,0 +1,6 @@
+extends Button
+
+
+func _on_Options_Button_pressed():
+	pass
+	get_tree().change_scene("Scenes/Menus/Options_Screen.tscn")
