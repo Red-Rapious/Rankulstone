f6a39e4409e6793bb5145d35708c4a37ccea5719
Red Rapious <red.rapious@gmail.com>
2019-11-08 20:35:49 +0100

    Working client-server

--- Godot Project/Scripts/Buttons/Create_Button.gd
+++ Godot Project/Scripts/Buttons/Create_Button.gd
@@ -1,15 +1,18 @@
 extends Button
 
-
-
-"""
 func _ready():
 	get_tree().connect("network_peer_connected", self, "_player_connected")
 	get_tree().connect("network_peer_disconnected", self, "_player_disconnected")
 
 func _on_Create_Button_pressed():
+	var port = 5000
+	var upnp = UPNP.new()
+	print(upnp.discover(2000, 2, "InternetGatewayDevice"))
+	print(upnp.get_gateway())
+	print(upnp.add_port_mapping(port))
+	
 	var peer = NetworkedMultiplayerENet.new()
-	peer.create_server(666, 2)
+	peer.create_server(port, 2)
 	get_tree().set_network_peer(peer)
 	print("Successfully created a server with IP: ", IP.get_local_addresses())
 	
@@ -19,7 +22,7 @@ func _player_connected(id):
 func _player_disconnected(id):
 	print("Player ", id, " leaved the game")
 
-"""
 
+"""
 func _on_Create_Button_pressed():
-	get_tree().change_scene("Scenes/Menus/Waiting_Screen.tscn")
+	get_tree().change_scene("Scenes/Menus/Waiting_Screen.tscn")"""
--- Godot Project/Scripts/Buttons/Join_Button.gd
+++ Godot Project/Scripts/Buttons/Join_Button.gd
@@ -1,13 +1,13 @@
 extends Button
 
-"""
+
 func _ready():
 	get_tree().connect("connected_to_server", self, "connected")
 	get_tree().connect("connection_failed", self, "failed")
 
 func _on_Join_Button_pressed():
 	var peer = NetworkedMultiplayerENet.new()
-	peer.create_client("192.168.0.30", 666)
+	peer.create_client("82.255.203.114", 5000)
 	get_tree().set_network_peer(peer)
 	
 
@@ -16,7 +16,11 @@ func connected():
 	
 func failed():
 	print("Connection failed...")
+	
+remote func sayhello():
+	print("Hello")
+	
 """
 
 func _on_Join_Button_pressed():
-	get_tree().change_scene("Scenes/Menus/Join_Screen.tscn")
+	get_tree().change_scene("Scenes/Menus/Join_Screen.tscn")"""
