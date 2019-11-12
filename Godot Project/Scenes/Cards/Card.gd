0cc521f6541a5d0f8e261271d64d570da125fbf5
Red Rapious <red.rapious@gmail.com>
2019-11-12 20:19:52 +0100

    First card skeleton
    Basic variables (export)

---
+++ Godot Project/Scenes/Cards/Card.gd
@@ -0,0 +1,14 @@
+extends Control
+
+export var NAME = "Nom de la carte"
+export var MANA_COST = 1
+export var IMAGE = ""
+export var RARITY = 0
+export var UNDER_TEXT = "\"Une carte vraiment qualitative\" - Un mec random"
+
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
