7d448319d2bdf3a8e9d03a44e414f428a810f737
Red Rapious <red.rapious@gmail.com>
2019-11-18 09:08:14 +0100

    Added a synchronized pv feature,not tested yet

--- Godot Project/Scripts/player.gd
+++ Godot Project/Scripts/player.gd
@@ -3,6 +3,8 @@ extends Node
 
 var mana_max = 10
 var mana = mana_max
+var self_pv = 30
+var opponent_pv
 var your_turn = true
 
 var opponent_hand_size = 7
@@ -13,8 +15,10 @@ var library = Array()
 
 signal self_hand_changed
 signal self_library_changed
+signal self_pv_changed
 signal opponent_hand_changed
 signal opponent_library_changed
+signal opponent_pv_changed
 
 	
 func init():
@@ -24,6 +28,7 @@ func init():
 	"""
 	connect("self_hand_changed", self, "_on_self_hand_changed")
 	connect("self_library_changed", self, "_on_self_library_changed")
+	connect("self_pv_changed", self, "_on_self_pv_changed")
 	for i in range(30):
 		library.append("Card")
 	draw_hand()
@@ -35,6 +40,16 @@ func _on_self_hand_changed():
 func _on_self_library_changed():
 	rpc("opponent_library_changed", len(library))
 
+func _on_self_pv_changed():
+	rpc("oppoenet_pv_changed", self_pv)
+
+func set_self_pv(new_value: int):
+	self_pv = new_value
+	emit_signal("self_pv_changed")
+	
+func add_self_pv(value: int):
+	self_pv += value
+	emit_signal("self_pv_changed")
 
 
 func draw_hand(size=7):
@@ -74,6 +89,13 @@ remote func opponent_library_changed(new_value: int):
 	"""
 	opponent_library_size = new_value
 	emit_signal("opponent_library_changed")
+	
+remote func opponent_pv_changed(new_value: int):
+	"""
+	Called by the player who pv on his opponent side
+	"""
+	opponent_pv = new_value
+	emit_signal("opponent_pv_changed")
 	
 	
 	
