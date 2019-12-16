22a676149080b45d9678c3987cbec049dd1d4203
Red Rapious <red.rapious@gmail.com>
2019-12-16 09:39:58 +0100

    Change deck_creation into deck_collection screen
    And start to play with grid_containers

--- Godot Project/Scripts/Menus/Deck_creation_Screen.gd
+++
@@ -1,6 +0,0 @@
-extends Control
-
-func _ready():
-	decks.save_deck("deck1")
-	$ProgressBar.set_value(5)
-
---
+++ Godot Project/Scripts/Menus/Deck_crollection_Screen.gd
@@ -0,0 +1,7 @@
+extends Control
+"""
+A screen to select the deck to modify in the Deck_modification_Screen
+"""
+
+func _ready():
+	decks.save_deck("deck1")
