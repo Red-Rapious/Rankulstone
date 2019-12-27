3cac66b576fcee46dfe7ec4a6e566669f2147029
Red Rapious <red.rapious@gmail.com>
2019-12-27 22:56:10 +0100

    Minor changes, good night

--- Godot Project/Scripts/Cards/Range_top.gd
+++ Godot Project/Scripts/Cards/Range_top.gd
@@ -7,7 +7,7 @@ func is_target_ok(target_id):
 	var scene = load("res://Scenes/Cards/"+player.uniq_ids_list[target_id][player.CREATURE_NAME]+".tscn")
 	var instance = scene.instance()
 	
-	return "Sbire" in instance.tribe
+	return "Miko" in instance.tribe
 
 func apply_effect_to_creature(creature_id):
 	player.kill_creature(creature_id) # kill the creature
