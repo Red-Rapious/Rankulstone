0491eee14c02f6dcd6abd3934a304aff44e28fff
Red Rapious <red.rapious@gmail.com>
2020-01-16 07:40:10 +0100

    Added minor changes to "Larme de la deesse", now working

--- Godot Project/Scripts/Autoloads/global.gd
+++ Godot Project/Scripts/Autoloads/global.gd
@@ -5,7 +5,7 @@ enum {PLAY, ATTACK} # differents drag types
 enum {SELF_CREATURE_DATA, OPPONENT_CREATURE_DATA} # for fight data, an array will be passed, containing both creatures data
 
 var card_index = [
-#"Larme_de_la_deesse",
+"Larme_de_la_deesse",
 "Reproduction_imparfaite",
 "Miko_le_BG",
 "Grosse_Berta",
--- Godot Project/Scripts/Cards/Larme_de_la_deesse.gd
+++ Godot Project/Scripts/Cards/Larme_de_la_deesse.gd
@@ -1,10 +1,10 @@
 extends "res://Scripts/Cards/Spell.gd"
 tool
 
-const ADD_MANA_VALUE = 2
+const ADD_MANA_VALUE = 1
 
 func _ready():
 	node_name = "Larme_de_la_deesse"
 
 func apply_effect():
-	player.add_self_mana(ADD_MANA_VALUE)
+	player.set_self_mana_max(player.self_mana + ADD_MANA_VALUE)
